b27c1b2dba6c2288fc7e9a7237ffde0d629739aa
Emerson <emersonlucenaoficial@gmail.com>
2021-08-10 16:13:43 -0300

    changes

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::get();
+        return view('admin.posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+        return redirect()->route('posts.index');
+    }
+}
