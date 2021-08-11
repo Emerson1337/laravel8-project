dfebf93c5e29c3f8ab02adc2126ef969117845f0
Emerson <emersonlucenaoficial@gmail.com>
2021-08-11 15:16:37 -0300

    finished frontend

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'Edição')
 
 @section('content')
-<h1>Editar o Post <strong>{{$post->title}}</strong></h1>
-
-<form action="{{route('posts.update', ['id' => $post->id])}}" method="post" enctype="multipart/form-data">
-    @method('PUT')
-    @include('admin.posts._partials.form')
-    <button type="submit">Editar</button>
-</form>
+<h1 class="text-center text-3xl uppercase font-black my-4">Editar o Post <strong>{{$post->title}}</strong></h1>
+<div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
+    <form action="{{route('posts.update', ['id' => $post->id])}}" method="post" enctype="multipart/form-data">
+        @method('PUT')
+        @include('admin.posts._partials.form')
+    </form>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,16 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
